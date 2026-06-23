b9c81eec3521d67dba9bdfdf1ea543676d2808af
raitocc <raitocc@qq.com>
2026-06-23 14:48:28 +0800

    重构：只保留AST构建阶段，彻底删除语义分析、中间代码生成及后端代码生成相关文件

--- src/main/java/com/toyc/compiler/Main.java
+++ src/main/java/com/toyc/compiler/Main.java
@@ -1,28 +1,15 @@
 package com.toyc.compiler;
 
 import com.toyc.compiler.ast.AST;
-import com.toyc.compiler.semantic.SemanticAnalyzer;
-import com.toyc.compiler.ir.TAC;
-import com.toyc.compiler.ir.IRBuilder;
-import com.toyc.compiler.optimize.Optimizer;
-import com.toyc.compiler.backend.RiscvCodeGen;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 import toyc.ToyCLexer;
 import toyc.ToyCParser;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        boolean optimize = false;
-        for (String arg : args) {
-            if ("-opt".equals(arg)) {
-                optimize = true;
-            }
-        }
-
         try {
             CharStream input = CharStreams.fromStream(System.in);
             
@@ -30,7 +17,7 @@ public class Main {
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             ToyCParser parser = new ToyCParser(tokens);
             
-            // Add custom error listener
+            // 添加自定义错误监听器
             parser.removeErrorListeners();
             parser.addErrorListener(new BaseErrorListener() {
                 @Override
@@ -43,20 +30,6 @@ public class Main {
             
             AstBuilder builder = new AstBuilder();
             AST.Node ast = builder.visit(tree);
-            
-            SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer();
-            AST.Node semanticAst = semanticAnalyzer.analyze(ast);
-            
-            IRBuilder irBuilder = new IRBuilder();
-            List<TAC> instructions = irBuilder.build(semanticAst);
-            
-            if (optimize) {
-                Optimizer optimizer = new Optimizer();
-                instructions = optimizer.optimize(instructions);
-            }
-            
-            RiscvCodeGen codeGen = new RiscvCodeGen(System.out, instructions);
-            codeGen.generate();
             
         } catch (Exception e) {
             System.err.println(e.getMessage());
